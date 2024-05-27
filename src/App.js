87f60722c34dc12485757106e492740174286b8a
AndreySavchenkov <sannndvik16@gmail.com>
2024-05-27 09:37:28 +0200

    feat: split-screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import SplitScreen from "./components/split-screen";
 
+const LeftSideComponent = ({ title }) => {
+  return <h2 style={{ backgroundColor: "rosybrown" }}>{title}</h2>;
+};
 
+const RightSideComponent = ({ title }) => {
+  return <h2 style={{ backgroundColor: "burlywood" }}>{title}</h2>;
+};
 
 function App() {
   return (
-    <div className="App">
-      start
-    </div>
+    <SplitScreen leftWidth={1} rightWidth={3}>
+      <LeftSideComponent title="Left" />
+      <RightSideComponent title="Right" />
+    </SplitScreen>
   );
 }
 
