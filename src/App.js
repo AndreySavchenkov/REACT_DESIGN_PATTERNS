318588e15c33051002fb0bc0e553121cb0de1284
AndreySavchenkov <sannndvik16@gmail.com>
2024-05-28 10:53:01 +0200

    feat: lists

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import SplitScreen from "./components/split-screen";
-
-const LeftSideComponent = ({ title }) => {
-  return <h2 style={{ backgroundColor: "rosybrown" }}>{title}</h2>;
-};
-
-const RightSideComponent = ({ title }) => {
-  return <h2 style={{ backgroundColor: "burlywood" }}>{title}</h2>;
-};
+import LargeAuthorListItems from "./components/authors/LargeListItems";
+import SmallAuthorListItems from "./components/authors/SmallListItems";
+import LargeBookListItems from "./components/books/LargeListItems";
+import SmallBookListItems from "./components/books/SmallListItems";
+import NumberedList from "./components/lists/Numbered";
+import RegularList from "./components/lists/Regular";
+import { authors } from "./data/authors";
+import { books } from "./data/books";
 
 function App() {
   return (
-    <SplitScreen leftWidth={1} rightWidth={3}>
-      <LeftSideComponent title="Left" />
-      <RightSideComponent title="Right" />
-    </SplitScreen>
+    <>
+      <RegularList
+        items={authors}
+        sourceName="author"
+        ItemComponent={SmallAuthorListItems}
+      />
+      <NumberedList
+        items={authors}
+        sourceName="author"
+        ItemComponent={LargeAuthorListItems}
+      />
+      <RegularList
+        items={books}
+        sourceName="book"
+        ItemComponent={SmallBookListItems}
+      />
+      <NumberedList
+        items={books}
+        sourceName="book"
+        ItemComponent={LargeBookListItems}
+      />
+    </>
   );
 }
 
---
+++ src/data/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    name: "To kill a Mockingbird",
+    pages: 281,
+    title: "Harper Lee",
+    price: 12.99,
+  },
+  {
+    name: "To Catcher in teh Rye",
+    pages: 224,
+    title: "J.D. Salinger",
+    price: 9.99,
+  },
+  {
+    name: "To Little Prince",
+    pages: 85,
+    title: "Antone de Saint-Exupéry",
+    price: 7.99,
+  },
+];
