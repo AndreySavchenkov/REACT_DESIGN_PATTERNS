6eed65f830793b98f5cb54601afc3b04e39b981d
AndreySavchenkov <sannndvik16@gmail.com>
2024-06-03 16:46:22 +0200

    feat: uncontrolled form

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import axios from "axios";
-import { UserInfo } from "./components/user-info";
-import { DataSourceWithRender } from "./components/data-source-wit-render";
-
-const getDataFromServer = async (url) => {
-  const response = await axios.get(url);
-  return response.data;
-};
-
-const getDataFromLocalStorage = (key) => {
-  return localStorage.getItem(key);
-};
-
-const Message = ({ msg }) => <h1>{msg}</h1>;
+import { UncontrolledForm } from "./components/uncontrolled-form";
 
 function App() {
   return (
     <>
-      <DataSourceWithRender
-        getData={() => getDataFromServer("/users/2")}
-        render={(resource) => <UserInfo user={resource} />}
-      />
-
-      <DataSourceWithRender
-        getData={() => getDataFromLocalStorage("test")}
-        render={(resource) => <Message msg={resource} />}
-      />
+      <UncontrolledForm />
     </>
   );
 }
