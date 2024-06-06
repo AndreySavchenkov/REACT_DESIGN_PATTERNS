a06fdacebf170726d1ac331d107fdb5083005bac
AndreySavchenkov <sannndvik16@gmail.com>
2024-06-06 17:56:07 +0200

    feat: collecting data

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import { UncontrolledFlow } from "./components/uncontrolled-flow";
 const StepOne = ({ goNext }) => {
   return (
     <>
-      <h1>Step #1</h1>
-      <button onClick={goNext}>Next</button>
+      <h1>Step #1: Enter Your Name</h1>
+      <button onClick={() => goNext({ name: "MyName" })}>Next</button>
     </>
   );
 };
 const StepTwo = ({ goNext }) => {
   return (
     <>
-      <h1>Step #2</h1>
-      <button onClick={goNext}>Next</button>
+      <h1>Step #2: Enter your age</h1>
+      <button onClick={() => goNext({ age: 34 })}>Next</button>
     </>
   );
 };
 const StepThree = ({ goNext }) => {
   return (
     <>
-      <h1>Step #3</h1>
-      <button onClick={goNext}>Next</button>
+      <h1>Step #3: Enter your country</h1>
+      <button onClick={() => goNext({ country: "Poland" })}>Next</button>
     </>
   );
 };
@@ -28,7 +28,12 @@ const StepThree = ({ goNext }) => {
 function App() {
   return (
     <>
-      <UncontrolledFlow>
+      <UncontrolledFlow
+        onDone={(data) => {
+          console.log(data);
+          alert("Yaee, you made it to the final step!");
+        }}
+      >
         <StepOne />
         <StepTwo />
         <StepThree />
