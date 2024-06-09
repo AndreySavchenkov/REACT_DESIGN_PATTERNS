7f0122fee3b7d5fc6fd07673b124b049fb664d7f
AndreySavchenkov <sannndvik16@gmail.com>
2024-06-09 16:39:58 +0200

    feat: create HOC logProps

--- src/App.js
+++ src/App.js
@@ -1,56 +1,12 @@
-import { useState } from "react";
-import { ControlledFlow } from "./components/controlled-flow";
+import { UserInfo } from "./components/user-info";
+import { logProps } from "./components/log-props";
 
-const StepOne = ({ goNext }) => {
-  return (
-    <>
-      <h1>Step #1: Enter Your Name</h1>
-      <button onClick={() => goNext({ name: "MyName" })}>Next</button>
-    </>
-  );
-};
-const StepTwo = ({ goNext }) => {
-  return (
-    <>
-      <h1>Step #2: Enter your age</h1>
-      <button onClick={() => goNext({ age: 34 })}>Next</button>
-    </>
-  );
-};
-const StepThree = ({ goNext }) => {
-  return (
-    <>
-      <h1>Congratulations! You quality for the gift!</h1>
-      <button onClick={() => goNext({})}>Next</button>
-    </>
-  );
-};
-const StepFourth = ({ goNext }) => {
-  return (
-    <>
-      <h1>Step #4: Enter your country</h1>
-      <button onClick={() => goNext({ country: "Poland" })}>Next</button>
-    </>
-  );
-};
+const UserInfoWrapper = logProps(UserInfo);
 
 function App() {
-  const [data, setData] = useState({});
-  const [currentStepIndex, setCurrentStepIndex] = useState(0);
-
-  const goNext = (dataFromStep) => {
-    setData({ ...data, ...dataFromStep });
-    setCurrentStepIndex(currentStepIndex + 1);
-  };
-
   return (
     <>
-      <ControlledFlow currentIndex={currentStepIndex} onNext={goNext}>
-        <StepOne />
-        <StepTwo />
-        {data.age > 25 && <StepThree />}
-        <StepFourth />
-      </ControlledFlow>
+      <UserInfoWrapper test={"test"} />
     </>
   );
 }
