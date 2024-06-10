bfb9699b1e60f3a604ac5c96a041c0ecc52fc9a8
AndreySavchenkov <sannndvik16@gmail.com>
2024-06-10 07:53:50 +0200

    feat: data loading with HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { UserInfo } from "./components/user-info";
-import { logProps } from "./components/log-props";
+import { includeUser } from "./components/include-user";
 
-const UserInfoWrapper = logProps(UserInfo);
+const UserInfoWithLoader = includeUser(UserInfo, "3");
 
 function App() {
   return (
     <>
-      <UserInfoWrapper test={"test"} />
+      <UserInfoWithLoader />
     </>
   );
 }
