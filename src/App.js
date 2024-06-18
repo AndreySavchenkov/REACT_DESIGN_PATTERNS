1d2bfba9600bbb879cfe8b1cc7192245e998cfa0
AndreySavchenkov <sannndvik16@gmail.com>
2024-06-18 17:41:33 +0200

    feat: react portal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import { ParentComponent } from "./components/parent";
-import mitt from "mitt";
-
-export const emitter = mitt();
+import { useState } from "react";
+import { createPortal } from "react-dom";
 
 function App() {
-  return <ParentComponent />;
+  const [show, setShow] = useState(false);
+
+  return (
+    <div style={{ position: "absolute", marginTop: "200px" }}>
+      <h1>Other Content</h1>
+      <button onClick={() => setShow(true)}>Show Message</button>
+      <Alert show={show} onClose={() => setShow(false)}>
+        A sample message to show.
+        <br />
+        Click it ti close
+      </Alert>
+    </div>
+  );
 }
+
+const Alert = ({ children, onClose, show }) => {
+  if (!show) return;
+
+  return createPortal(
+    <div className="alert" onClick={onClose}>
+      {children}
+    </div>,
+    document.querySelector("#alert-holder")
+  );
+};
 
 export default App;
