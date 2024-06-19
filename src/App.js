d3dc8f9545d1a21a06784d52b33268ecf355281e
AndreySavchenkov <sannndvik16@gmail.com>
2024-06-19 17:15:58 +0200

    feat: forwarding refs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import { useState } from "react";
-import { createPortal } from "react-dom";
+import { useRef } from "react";
+import { Input } from "./components/input";
 
 function App() {
-  const [show, setShow] = useState(false);
+  const inputRef = useRef();
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+    console.log(inputRef.current.value);
+  }
 
   return (
-    <div style={{ position: "absolute", marginTop: "200px" }}>
-      <h1>Other Content</h1>
-      <button onClick={() => setShow(true)}>Show Message</button>
-      <Alert show={show} onClose={() => setShow(false)}>
-        A sample message to show.
-        <br />
-        Click it ti close
-      </Alert>
-    </div>
+    <form onSubmit={submitHandler}>
+      <Input ref={inputRef} className="text-input" />
+      <button type="submit" className="submit">
+        Submit
+      </button>
+    </form>
   );
 }
-
-const Alert = ({ children, onClose, show }) => {
-  if (!show) return;
-
-  return createPortal(
-    <div className="alert" onClick={onClose}>
-      {children}
-    </div>,
-    document.querySelector("#alert-holder")
-  );
-};
 
 export default App;
